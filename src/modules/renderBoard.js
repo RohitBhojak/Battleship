e056ecc17107d11b11520810a457d03423a44e9c
Rohit Bhojak <rohitbhojak99@gmail.com>
2025-09-15 13:25:12 +0000

    Improve renderBoard to render ship status

--- src/modules/renderBoard.js
+++ src/modules/renderBoard.js
@@ -1,5 +1,5 @@
-export default function renderBoard(board, gameBoard, isEnemy = false) {
-  board.innerHTML = "";
+export default function renderBoard(boardDisplay, gameBoard, isEnemy = false) {
+  boardDisplay.innerHTML = "";
   for (let i = 0; i < 10; i++) {
     for (let j = 0; j < 10; j++) {
       const cell = document.createElement("div");
@@ -7,11 +7,18 @@ export default function renderBoard(board, gameBoard, isEnemy = false) {
       cell.dataset.x = i;
       cell.dataset.y = j;
       // show player ships
-      if (!isEnemy && gameBoard.board[i][j] !== null) {
+      if (!isEnemy && gameBoard.board[i][j]) {
         cell.classList.add("ship");
       }
-      board.appendChild(cell);
+      // highlight hit cell
+      if (gameBoard.isAttacked(i, j)) {
+        cell.classList.add("hit");
+      }
+      // highlight sunk ship
+      if (gameBoard.board[i][j] && gameBoard.board[i][j].isSunk()) {
+        cell.classList.add("sunk");
+      }
+      boardDisplay.appendChild(cell);
     }
   }
-  return board;
 }
